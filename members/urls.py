b723ef21bf7ef55f28049eb330b0b57a2463c918
Seyedjamil Sabbaghifaragard <105069908+jamiljann@users.noreply.github.com>
2023-07-31 13:21:15 +0330

    Delete members directory

--- members/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from .import views
-
-
-urlpatterns = [    
-    path('',            views.login_user,            name="login"),
-    path('login_user',  views.login_user,            name="login"),
-    path('logout_user', views.logout_user,           name="logout"),  
-]
